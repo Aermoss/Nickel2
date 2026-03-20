6012261975d5194a1dae9cb11fc31c6a25d212f3
Yusuf <86446300+Aermoss@users.noreply.github.com>
2026-03-21 02:43:07 +0300

    Add setup script for MSYS2.

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+pacman -Syu --noconfirm && pacman -S --needed --noconfirm \
+    mingw-w64-ucrt-x86_64-toolchain \
+    mingw-w64-ucrt-x86_64-make \
+    mingw-w64-ucrt-x86_64-cmake \
+    mingw-w64-ucrt-x86_64-glfw \
+    mingw-w64-ucrt-x86_64-assimp \
+    mingw-w64-ucrt-x86_64-ffmpeg \
+    mingw-w64-ucrt-x86_64-entt \
+    mingw-w64-ucrt-x86_64-openal \
+    mingw-w64-ucrt-x86_64-freealut \
+    mingw-w64-ucrt-x86_64-glm
