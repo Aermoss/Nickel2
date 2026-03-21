ff5723ece44c55e1fd0f737b681ecdaedc2d7e63
Yusuf <86446300+Aermoss@users.noreply.github.com>
2026-03-21 06:01:46 +0300

    Update 'setup.sh'.

--- setup.sh
+++ setup.sh
@@ -10,4 +10,5 @@ pacman -Syu --noconfirm && pacman -S --needed --noconfirm \
     mingw-w64-ucrt-x86_64-entt \
     mingw-w64-ucrt-x86_64-openal \
     mingw-w64-ucrt-x86_64-freealut \
-    mingw-w64-ucrt-x86_64-glm
+    mingw-w64-ucrt-x86_64-glm \
+    mingw-w64-ucrt-x86_64-stb
